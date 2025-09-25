6fc4f0208e5c45abfada97c0955fb3468f235835
minh7709 <minh2k32003@gmail.com>
2025-09-26 03:02:25 +0700

    build dashboard and doing-homework page for student

---
+++ labs/lab07/sql/schema.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE IF NOT EXISTS e_learning;
+USE e_learning;
+
+-- Bảng tài khoản sinh viên
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    fullname VARCHAR(100)
+);
+
+-- Bảng lecture mà sinh viên đã có
+CREATE TABLE student_lectures (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    lecture_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (lecture_id) REFERENCES lectures(lecture_id)
+);
+
+
+CREATE TABLE lectures (
+    lecture_id INT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(20) UNIQUE NOT NULL,
+    title VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE questions (
+    question_id INT AUTO_INCREMENT PRIMARY KEY,
+    lecture_id INT NOT NULL,
+    content TEXT NOT NULL,
+    answer_a VARCHAR(255),
+    answer_b VARCHAR(255),
+    answer_c VARCHAR(255),
+    answer_d VARCHAR(255),
+    correct_answer CHAR(1),
+    FOREIGN KEY (lecture_id) REFERENCES lectures(lecture_id) ON DELETE CASCADE
+);
+
+INSERT INTO users (username, password, fullname)
+VALUES ('sv001', '123456', 'Nguyen Van A');
+
+INSERT INTO lectures (code, title)
+VALUES ('LEC101', 'Basic Math and Geography');
+
+INSERT INTO questions (lecture_id, content, answer_a, answer_b, answer_c, answer_d, correct_answer)
+VALUES
+(1, 'What is 2 + 2?', '3', '4', '5', '6', 'B'),
+(1, 'What is the capital of France?', 'Berlin', 'Paris', 'Rome', 'Madrid', 'B');
